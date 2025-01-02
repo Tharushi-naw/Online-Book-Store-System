87dbfdc307bdc15103b5bfbb3874bdabf03ee5ae
Tharushi_Nawodya <159925910+Tharushi-naw@users.noreply.github.com>
2025-01-02 23:42:10 +0530

    Updated getters and setters

--- src/Order.java
+++ src/Order.java
@@ -4,11 +4,50 @@ public class Order {
     private int quantity;
     private String customerName;
 
-    //Constructor
+    //Constructor to initialize order attributes
     public Order(int orderId, String bookTitle, int quantity, String customerName){
         this.orderId = orderId;
         this.bookTitle = bookTitle;
         this.quantity = quantity;
+        this.customerName = customerName;
+    }
+    //Getter for orderId
+    public int getOrderId(){
+        return orderId;
+    }
+
+    //Setter for orderId
+    public void setOrderId(int orderId){
+        this.orderId = orderId;
+    }
+
+    //Getter for book title
+    public String getBookTitle(){
+        return bookTitle;
+    }
+
+    //Setter for book title
+    public void setBookTitle(String bookTitle){
+        this.bookTitle = bookTitle;
+    }
+
+    //Getter for quantity
+    public int getQuantity(){
+        return quantity;
+    }
+
+    //Setter for quantity
+    public void setQuantity(int quantity){
+        this.quantity = quantity;
+    }
+
+    //Getter for customer name
+    public String getCustomerName(){
+        return customerName;
+    }
+
+    //Setter for customer name
+    public void setCustomerName(String customewrName){
         this.customerName = customerName;
     }
 }
