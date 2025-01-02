8e6a8a3422c56c4be5186535088d2fef3d35497f
Tharushi_Nawodya <159925910+Tharushi-naw@users.noreply.github.com>
2025-01-02 22:55:26 +0530

    Update PremiumCustomer.java
    Added getters and setters

--- src/PremiumCustomer.java
+++ src/PremiumCustomer.java
@@ -1,2 +1,20 @@
-public class PremiumCustomer {
+public class PremiumCustomer extends Customer {
+    //Attribute
+    private double discountRate;
+
+    //Constructor to initialize PremiumCustomer attributes
+    public PremiumCustomer(String name, String username, String password, double discountRate){
+        super(name, username, password);    //call parent constructor
+        this.discountRate= discountRate;
+    }
+
+    //Getter for discount rate
+    public double getDiscountRate(){
+        return discountRate;
+    }
+
+    //Setter for discount rate
+    public void setDiscountRate (double discountRate){
+        this.discountRate = discountRate;
+    }
 }
