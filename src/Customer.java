a8b3d8be3761705314efd0cd2880ba91e69c0f9d
Tharushi_Nawodya <159925910+Tharushi-naw@users.noreply.github.com>
2025-01-02 16:19:08 +0530

    Updated
    Added attributes and a constructor

--- src/Customer.java
+++ src/Customer.java
@@ -1,2 +1,14 @@
 public class Customer {
+    //Attributes
+    private String name;
+    private String username;
+    private String password;
+
+    //Constructor to initialize customer attributes
+    public Customer(String name, String username, String password){
+        this.name = name;
+        this.username = username;
+        this.password = password;
+    }
+
 }
