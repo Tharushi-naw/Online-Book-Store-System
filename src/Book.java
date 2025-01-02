b621535a8514a29c40d2f6e98df8daab4f87d1cc
Tharushi_Nawodya <159925910+Tharushi-naw@users.noreply.github.com>
2025-01-02 15:07:00 +0530

    Updated classes

---
+++ src/Book.java
@@ -0,0 +1,15 @@
+public class Book {
+    // Attributes of the Book class
+    private String title;
+    private String author;
+    private double price;
+    private int quantity;
+
+    //Constructor to initialize Book attributes
+    public Book(String title,String author,double price, int quantity) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
