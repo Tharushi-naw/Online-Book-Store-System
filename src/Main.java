63e1439a1833f74275e2cd6d57f16f062156d3de
Tharushi_Nawodya <159925910+Tharushi-naw@users.noreply.github.com>
2025-01-04 13:25:09 +0530

    Added Test cases

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        //Initialize rge bookstore and order manager
+        BookStore store = new BookStore();
+        OrderManager manager = new OrderManager();
+
+        //add books to the store
+        store.addBook(new Book("Java", "Doe", 49.99, 14));
+        store.addBook(new Book("Data Structures", "smith", 55.99, 4));
+
+        //Test Cases
+        System.out.println("----Test cases----");
+
+        //Test 1: place an order successfully
+        manager.placeOrder(store, "Java", 3);
+
+        //Test 2: attempt to order a book not in stock
+        manager.placeOrder(store, "Python", 1);
+
+        //Test 3: attempt to order more than available quantity
+        manager.placeOrder(store, "Data Structures", 6);
     }
 }
